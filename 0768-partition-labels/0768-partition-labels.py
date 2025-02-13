68edc9fe152453db60a61546d3b260dd31462a85
Ramsi97 <ramadan2jemal@gmail.com>
2025-02-13 09:47:56 +0300

    Time: 4 ms (63.55%), Space: 17.8 MB (54.09%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -5,14 +5,12 @@ class Solution:
         last_index = {}
         for i in range(n):
             last_index[s[i]] = i
-        print(last_index)
         left = 0
         right = 0
         result = []
         end = 0
         while right < n:
             end = max(end, last_index[s[right]])
-            print(right, last_index[s[right]])
             if right == end:
                 print(s[i])
                 result.append(right-left+1)
